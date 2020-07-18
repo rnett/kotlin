bf2bfa159bbc3ac4608f701b06254b8d87cc027e
Arsen Nagdalian <nagoarsen@mail.ru>
2020-07-19 02:08:01 +0300

    Add a Fir node that wraps a ConeEffectDeclaration

--- compiler/fir/tree/gen/org/jetbrains/kotlin/fir/visitors/FirVisitorVoid.kt
+++ compiler/fir/tree/gen/org/jetbrains/kotlin/fir/visitors/FirVisitorVoid.kt
@@ -126,6 +126,7 @@ import org.jetbrains.kotlin.fir.types.FirFunctionTypeRef
 import org.jetbrains.kotlin.fir.types.FirResolvedFunctionTypeRef
 import org.jetbrains.kotlin.fir.types.FirImplicitTypeRef
 import org.jetbrains.kotlin.fir.types.FirComposedSuperTypeRef
+import org.jetbrains.kotlin.fir.contracts.FirEffectDeclaration
 import org.jetbrains.kotlin.fir.contracts.FirContractDescription
 import org.jetbrains.kotlin.fir.contracts.FirRawContractDescription
 import org.jetbrains.kotlin.fir.contracts.FirResolvedContractDescription
@@ -616,6 +617,10 @@ abstract class FirVisitorVoid : FirVisitor<Unit, Nothing?>() {
 
     open fun visitComposedSuperTypeRef(composedSuperTypeRef: FirComposedSuperTypeRef) {
         visitElement(composedSuperTypeRef)
+    }
+
+    open fun visitEffectDeclaration(effectDeclaration: FirEffectDeclaration) {
+        visitElement(effectDeclaration)
     }
 
     open fun visitContractDescription(contractDescription: FirContractDescription) {
@@ -1112,6 +1117,10 @@ abstract class FirVisitorVoid : FirVisitor<Unit, Nothing?>() {
 
     final override fun visitComposedSuperTypeRef(composedSuperTypeRef: FirComposedSuperTypeRef, data: Nothing?) {
         visitComposedSuperTypeRef(composedSuperTypeRef)
+    }
+
+    final override fun visitEffectDeclaration(effectDeclaration: FirEffectDeclaration, data: Nothing?) {
+        visitEffectDeclaration(effectDeclaration)
     }
 
     final override fun visitContractDescription(contractDescription: FirContractDescription, data: Nothing?) {
