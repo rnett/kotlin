8ef688c7eefa301cb86466f8d81050a52d1b11f6
Arsen Nagdalian <arsen.nagdalian@jetbrains.com>
2020-07-19 03:46:05 +0300

    Add isContract flag to Fir declaration status

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/NodeConfigurator.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/NodeConfigurator.kt
@@ -328,7 +328,7 @@ object NodeConfigurator : AbstractFieldConfigurator<FirTreeBuilder>(FirTreeBuild
             generateBooleanFields(
                 "expect", "actual", "override", "operator", "infix", "inline", "tailRec",
                 "external", "const", "lateInit", "inner", "companion", "data", "suspend", "static",
-                "fromSealedClass", "fromEnumClass", "fun"
+                "fromSealedClass", "fromEnumClass", "fun", "contract"
             )
         }
 
