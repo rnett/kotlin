c94f0ae54aab4c0f355cc4a5e065b5d921dfbd1d
Alexander Udalov <alexander.udalov@jetbrains.com>
2020-07-22 18:41:14 +0200

    IR: push down implementations of startOffset/endOffset/type/...
    Do not store these as properties in IrElementBase, IrExpressionBase and
    similar classes. This helps to reduce boilerplate in implementations
    (just "override val" in the constructor, instead of taking a parameter
    and passing it to the superclass), and also slightly optimizes memory in
    cases where the value is trivial (UNDEFINED_OFFSET, 0, etc) and thus
    does not need to be stored.

--- compiler/ir/ir.tree.impl/src/org/jetbrains/kotlin/ir/expressions/impl/IrBlockBodyImpl.kt
+++ compiler/ir/ir.tree.impl/src/org/jetbrains/kotlin/ir/expressions/impl/IrBlockBodyImpl.kt
@@ -22,7 +22,10 @@ import org.jetbrains.kotlin.ir.declarations.IrDeclarationFactory
 import org.jetbrains.kotlin.ir.declarations.impl.IrDeclarationFactoryImpl
 import org.jetbrains.kotlin.ir.expressions.IrBlockBody
 
-class IrBlockBodyImpl(startOffset: Int, endOffset: Int) : IrElementBase(startOffset, endOffset), IrBlockBody {
+class IrBlockBodyImpl(
+    override val startOffset: Int,
+    override val endOffset: Int
+) : IrElementBase(), IrBlockBody {
     constructor(startOffset: Int, endOffset: Int, statements: List<IrStatement>) : this(startOffset, endOffset) {
         this.statements.addAll(statements)
     }
